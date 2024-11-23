fb9054f06350090942636ae5d733662c34edb0ef
Agustina Cabello <cabelloagus000@gmail.com>
2024-11-22 23:50:02 -0300

    contenedor para products

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
 
-  validates :name, presense: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Category.create(name: 'Shoes')
+Category.create(name: 'Clothing')
+Category.create(name: 'Accessories')
+
+Color.create(name: 'Red')
+Color.create(name: 'Blue')
+Color.create(name: 'Black')
+Color.create(name: 'White')
